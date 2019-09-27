a07ad40331ee1fab040d4993c87ef1db6d63e5fa
Paulo Correa <paulo.correa@indt.org.br>
2019-09-27 13:15:28 -0400

    add ResponseEntity

--- src/main/java/com/pcorrea/workshopmongo/resources/UserResource.java
+++ src/main/java/com/pcorrea/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.pcorrea.workshopmongo.resources;
 
 import com.pcorrea.workshopmongo.domain.User;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +14,13 @@ import java.util.List;
 @RequestMapping(value="/users") //para informar o caminho do endpoint
 public class UserResource {
     @RequestMapping(method = RequestMethod.GET) // para dizer que esse metodo será RES no caminho /users == @GetMapping
-    public  List<User> findAll(){
+    public ResponseEntity <List<User>> findAll(){ //ResponseEntity para retornar respostas http e outros dados corrigidos ou erros
         User paulo = new User("1", "Paulo Correa","pc.rochacorrea@gmail.com");
         User mel = new User("2", " Mel Guilia","mel.guilia@gmail.com");
         User maria = new User("3", "Maria Brown","maria@gmail.com");
         User giselle = new User("4", "Giselle Beatriz","giselle.beatriz@gmail.com");
         List<User> list = new ArrayList<>();
         list.addAll(Arrays.asList(paulo,mel,maria,giselle));
-        return list;
+        return ResponseEntity.ok().body(list);
     }
 }
