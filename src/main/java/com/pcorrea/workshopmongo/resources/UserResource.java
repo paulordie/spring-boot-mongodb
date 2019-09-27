1f28f60f91c94461292622274f7a41645f8c21d4
Paulo Correa <paulo.correa@indt.org.br>
2019-09-27 13:13:36 -0400

    create User&UserResource REST

---
+++ src/main/java/com/pcorrea/workshopmongo/resources/UserResource.java
@@ -0,0 +1,25 @@
+package com.pcorrea.workshopmongo.resources;
+
+import com.pcorrea.workshopmongo.domain.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController //para informar que essa classe será um recurso REST
+@RequestMapping(value="/users") //para informar o caminho do endpoint
+public class UserResource {
+    @RequestMapping(method = RequestMethod.GET) // para dizer que esse metodo será RES no caminho /users == @GetMapping
+    public  List<User> findAll(){
+        User paulo = new User("1", "Paulo Correa","pc.rochacorrea@gmail.com");
+        User mel = new User("2", " Mel Guilia","mel.guilia@gmail.com");
+        User maria = new User("3", "Maria Brown","maria@gmail.com");
+        User giselle = new User("4", "Giselle Beatriz","giselle.beatriz@gmail.com");
+        List<User> list = new ArrayList<>();
+        list.addAll(Arrays.asList(paulo,mel,maria,giselle));
+        return list;
+    }
+}
